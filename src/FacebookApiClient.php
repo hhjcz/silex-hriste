549918d5f4409d68f94d5894274d7cfb47b8b112
hhjcz <hhj@centrum.cz>
2015-04-17 17:28:52 +0200

    First functional facebook list

--- src/FacebookApiClient.php
+++ src/FacebookApiClient.php
@@ -1,5 +1,6 @@
 <?php
 
+use Facebook\FacebookJavaScriptLoginHelper;
 use Facebook\FacebookRedirectLoginHelper;
 use Facebook\FacebookRequest;
 use Facebook\FacebookRequestException;
@@ -15,15 +16,11 @@ class FacebookApiClient {
 
 	/** @var FacebookRedirectLoginHelper */
 	private $fbHelper;
-
-	public function __construct()
-	{
-	}
+	/** @var  FacebookSession */
+	private $session;
 
 	public function authenticate()
 	{
-		// sebrano z: http://www.finalwebsites.com/facebook-api-php-tutorial/
-
 		$api_key = '729201217200960';
 		$api_secret = '6df300016ba93013098294136a836be8';
 		$redirect_login_url = 'http://localhost:8888/facebook';
@@ -31,126 +28,158 @@ class FacebookApiClient {
 		// initialize your app using your key and secret
 		FacebookSession::setDefaultApplication($api_key, $api_secret);
 
-		// create a helper opject which is needed to create a login URL
+		// create a helper object which is needed to create a login URL
 		// the $redirect_login_url is the page a visitor will come to after login
-		$this->fbHelper = new FacebookRedirectLoginHelper($redirect_login_url);
+		$this->fbHelper = new FacebookJavaScriptLoginHelper($redirect_login_url);
 
 		// First check if this is an existing PHP session
 		if (isset($_SESSION) && isset($_SESSION['fb_token']))
 		{
-			// create new session from the existing PHP sesson
-			$session = new FacebookSession($_SESSION['fb_token']);
+			// create new session from the existing PHP session
+			$this->session = new FacebookSession($_SESSION['fb_token']);
 			try
 			{
 				// validate the access_token to make sure it's still valid
-				if ( ! $session->validate()) $session = null;
+				if ( ! $this->session->validate()) $this->session = null;
 			} catch (Exception $e)
 			{
 				// catch any exceptions and set the sesson null
-				$session = null;
+				$this->session = null;
 				echo 'No session: ' . $e->getMessage();
-				//throw $e;
+				throw $e;
 			}
-		} elseif (empty($session))
+		} elseif (empty($this->session))
 		{
 			// the session is empty, we create a new one
 			try
 			{
 				// the visitor is redirected from the login, let's pickup the session
-				$session = $this->fbHelper->getSessionFromRedirect();
+				$this->session = $this->fbHelper->getSession();
 			} catch (FacebookRequestException $e)
 			{
 				// Facebook has returned an error
-				echo 'Facebook (session) request error: ' . $e->getMessage();
-				//throw $e;
+				//echo 'Facebook (session) request error: ' . $e->getMessage();
+				throw $e;
 			} catch (Exception $e)
 			{
 				// Any other error
-				echo 'Other (session) request error: ' . $e->getMessage();
+				//echo 'Other (session) request error: ' . $e->getMessage();
+				throw $e;
 			}
 		}
 
-		if (isset($session))
+		if (isset($this->session))
 		{
 			// store the session token into a PHP session
-			$_SESSION['fb_token'] = $session->getToken();
-			// and create a new Facebook session using the cururent token
+			$_SESSION['fb_token'] = $this->session->getToken();
+			// and create a new Facebook session using the current token
 			// or from the new token we got after login
-			$session = new FacebookSession($session->getToken());
-			try
-			{
-				// with this session I will post a message to my own timeline
-				//$request = new FacebookRequest(
-				//	$session,
-				//	'POST',
-				//	'/me/feed',
-				//	array(
-				//		'link'    => 'www.finalwebsites.com/facebook-api-php-tutorial/',
-				//		'message' => 'A step by step tutorial on how to use Facebook PHP SDK v4.0'
-				//	)
-				//);
-				/** @var GraphObject $graphObject */
-				$request = new FacebookRequest($session, 'GET', '/me');
-				$response = $request->execute();
-				$graphObject = $response->getGraphObject(GraphUser::className());
-				// the POST response object
-				// echo '<pre>' . print_r($graphObject, 1) . '</pre>';
-				//$msgid = $graphObject->getProperty('id');
-				dump($graphObject);
-
-				$request = new FacebookRequest($session, 'GET', '/me/notifications');
-				$response = $request->execute();
-				$graphObject = $response->getGraphObject();
-				dump($graphObject->getProperty('summary'));//->getProperty('unseen_count'));
-				dump($graphObject->getPropertyAsArray('data'));
-				//dump($graphObject->getPropertyNames());
-
-				$request = new FacebookRequest($session, 'GET', '/me/inbox', ['limit' => 3000]);
-				$response = $request->execute();
-				$graphObject = $response->getGraphObject();
-				dump($graphObject);
-				dump($graphObject->getPropertyAsArray('data'));
-
-				$request = new FacebookRequest($session, 'GET', '/390523424453383');
-				$response = $request->execute();
-				$graphObject = $response->getGraphObject();
-				dump($graphObject);
-				dump($graphObject->getPropertyAsArray('data'));
-			} catch (FacebookRequestException $e)
-			{
-				// show any error for this facebook request
-				echo 'Facebook (post) request error: ' . $e->getMessage();
-			}
-			if (isset ($msgid))
-			{
-				// we only need to the sec. part of this ID
-				$parts = explode('_', $msgid);
-				try
-				{
-					$request2 = new FacebookRequest(
-						$session,
-						'GET',
-						'/' . $parts[1]
-					);
-					$response2 = $request2->execute();
-					$graphObject2 = $response2->getGraphObject();
-					// the GET response object
-					echo '<pre>' . print_r($graphObject2, 1) . '</pre>';
-				} catch (FacebookRequestException $e)
-				{
-					// show any error for this facebook request
-					echo 'Facebook (get) request error: ' . $e->getMessage();
-				}
-			}
+			//$this->session = new FacebookSession($this->session->getToken());
 		} else
 		{
 			// we need to create a new session, provide a login link
-			//echo 'No session, please <a href="' . $this->fbHelper->getLoginUrl(array('publish_actions')) . '">login</a>.';
+			echo 'No session, please <a href="' . $this->fbHelper->getLoginUrl(array('publish_actions')) . '">login</a>.';
 		}
+
+		return $this->session;
 	}
 
 	public function fbHelper()
 	{
 		return $this->fbHelper;
+	}
+
+	/**
+	 * @param string $userId
+	 * @return GraphUser
+	 * @throws FacebookRequestException
+	 */
+	public function getUserInfo($userId = 'me')
+	{
+		try
+		{
+			/** @var GraphObject $graphObject */
+			$request = new FacebookRequest($this->session, 'GET', '/' . $userId);
+			$response = $request->execute();
+			$graphObject = $response->getGraphObject(GraphUser::className());
+		} catch (FacebookRequestException $e)
+		{
+			// show any error for this facebook request
+			//echo 'Facebook (post) request error: ' . $e->getMessage();
+			throw $e;
+		}
+		return $graphObject;
+	}
+
+	/**
+	 * @return GraphObject
+	 * @throws FacebookRequestException
+	 */
+	public function getNotifications()
+	{
+		try
+		{
+			$request = new FacebookRequest($this->session, 'GET', '/me/notifications');
+			$response = $request->execute();
+			$graphObject = $response->getGraphObject();
+		} catch (FacebookRequestException $e)
+		{
+			throw $e;
+		}
+
+		return $graphObject;
+	}
+
+	/**
+	 * @return GraphObject
+	 * @throws FacebookRequestException
+	 */
+	public function getMessages()
+	{
+		try
+		{
+			$request = new FacebookRequest($this->session, 'GET', '/me/inbox', ['limit' => 5]);
+			$response = $request->execute();
+			$messagesGraph = $response->getGraphObject();
+			//dump($messagesGraph);
+			$messages = new stdClass();
+			$messages->summary = $messagesGraph->getProperty('summary');
+			$messages->unseen = $messages->summary->getProperty('unseen_count');
+			$messages->unread = $messages->summary->getProperty('unread_count');
+			foreach ($messagesGraph->getPropertyAsArray('data') as $threadGraph)
+			{
+				$thread['unseen'] = $threadGraph->getProperty('unseen');
+				$thread['unread'] = $threadGraph->getProperty('unread');
+
+				$threadUsers = '';
+				$usersGraph = $threadGraph->getProperty('to');
+				foreach ($usersGraph as $userGraph)
+				{
+					$threadUsers .= $userGraph->getProperty('name') . ', ';
+				}
+				$thread['users'] = '';
+				$thread['users'] .= '' . $usersGraph->getProperty('0')->getProperty('name');
+				$thread['users'] .= ', ' . $usersGraph->getProperty('1')->getProperty('name');
+				$thread['users'] .= $usersGraph->getProperty('2') ? ', ' . $usersGraph->getProperty('2')->getProperty('name') : '';
+
+				$messagesInThreadGraph = $threadGraph->getProperty('comments')->getPropertyAsArray('data');
+				$messagesInThread = [];
+				foreach ($messagesInThreadGraph as $messagesInThreadGraph)
+				{
+					$messageInThread['from'] = $messagesInThreadGraph->getProperty('from')->getProperty('name');
+					$messageInThread['created_time'] = $messagesInThreadGraph->getProperty('created_time');
+					$messageInThread['message'] = $messagesInThreadGraph->getProperty('message');
+					array_unshift($messagesInThread, $messageInThread);
+				}
+				$thread['messages'] = $messagesInThread;
+				$messages->threads[] = $thread;
+			}
+		} catch (FacebookRequestException $e)
+		{
+			throw $e;
+		}
+
+		//dump($messages);
+		return $messages;
 	}
 }
--- src/controllers.php
+++ src/controllers.php
@@ -2,40 +2,55 @@
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 //Request::setTrustedProxies(array('127.0.0.1'));
 
-$app->get('/', function () use ($app) {
-    return $app['twig']->render('index.html', array());
+$app->get('/', function () use ($app)
+{
+	return $app['twig']->render('index.html', array());
 })
-->bind('homepage')
-;
+	->bind('homepage');
 
-$app->get('/facebook', function () use ($app) {
-    session_start();
-    $fbClient = new FacebookApiClient();
-    $fbClient->authenticate();
-    $loginUrl = $fbClient->fbHelper()->getLoginUrl(['manage_notifications', 'read_mailbox']);
-    //return view('hriste', ['loginUrl' => $loginUrl]);
-    return $app['twig']->render('facebook.html', array('loginUrl' => $loginUrl));
+$app->get('/facebook', function () use ($app)
+{
+	session_start();
+	$fbClient = new FacebookApiClient();
+	$fbSession = $fbClient->authenticate();
+	$loginUrl = '';
+	$logoutUrl = '';
+	//$loginUrl = $fbClient->fbHelper()->getLoginUrl(['manage_notifications', 'read_mailbox']);
+	//$logoutUrl = $fbClient->fbHelper()->getLogoutUrl($fbSession, 'http://localhost:8888/facebook');
+
+	$me = $fbClient->getUserInfo();
+	$fbUsername = $me->getName();
+	$fbId = $me->getId();
+	$messages = $fbClient->getMessages();
+
+	return $app['twig']->render('facebook.html', array(
+		'userLoggedIn' => true,
+		'loginUrl'     => $loginUrl,
+		'logoutUrl'    => $logoutUrl,
+		'fbUserName'   => $fbUsername,
+		'userLoggedIn' => true,
+		'fbId'         => $fbId,
+		'messages'     => $messages
+	));
 });
 
+$app->error(function (\Exception $e, Request $request, $code) use ($app)
+{
+	if ($app['debug'])
+	{
+		return;
+	}
 
-$app->error(function (\Exception $e, Request $request, $code) use ($app) {
-    if ($app['debug']) {
-        return;
-    }
+	// 404.html, or 40x.html, or 4xx.html, or error.html
+	$templates = array(
+		'errors/' . $code . '.html',
+		'errors/' . substr($code, 0, 2) . 'x.html',
+		'errors/' . substr($code, 0, 1) . 'xx.html',
+		'errors/default.html',
+	);
 
-    // 404.html, or 40x.html, or 4xx.html, or error.html
-    $templates = array(
-        'errors/'.$code.'.html',
-        'errors/'.substr($code, 0, 2).'x.html',
-        'errors/'.substr($code, 0, 1).'xx.html',
-        'errors/default.html',
-    );
-
-    return new Response($app['twig']->resolveTemplate($templates)->render(array('code' => $code)), $code);
+	return new Response($app['twig']->resolveTemplate($templates)->render(array('code' => $code)), $code);
 });
