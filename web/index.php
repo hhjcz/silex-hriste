84b0a6fb4c23baa0f8eb1ac06d98ebfdc9e8b298
hhjcz@hhj-nb <hhj@centrum.cz>
2015-05-04 15:43:57 +0200

    Dummy fake commit for testing branching

--- web/index.php
+++ web/index.php
@@ -2,6 +2,7 @@
 
 use Symfony\Component\Debug\Debug;
 
+
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
