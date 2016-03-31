053808390a9c867827160e641f7fae224a3b3799
Jan Haering <hhj@centrum.cz>
2016-03-31 10:23:25 +0200

    Env support for heroku

--- config/prod.php
+++ config/prod.php
@@ -8,7 +8,11 @@ $app['twig.path'] = array(__DIR__.'/../templates');
 // hhj: disable cache even in prod env:
 //$app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
 
-require __DIR__ . '/../env.php';
+$app['fb_api_key'] = getenv('fb_api_key');
+$app['fb_api_secret'] = getenv('fb_api_secret');
+$app['fb_redirect_login_url'] = getenv('fb_redirect_login_url');
+
+include __DIR__ . '/../env.php';
 
 $app->register(new MonologServiceProvider(), array(
 	'monolog.logfile' => __DIR__.'/../var/logs/silex_dev.log',
