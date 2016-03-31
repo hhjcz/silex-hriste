4c0fb8dae29d37c28188a6a571f85525f42f56e4
Jan Haering <hhj@centrum.cz>
2016-03-31 10:25:27 +0200

    Env support for heroku

--- config/prod.php
+++ config/prod.php
@@ -12,7 +12,8 @@ $app['fb_api_key'] = getenv('fb_api_key');
 $app['fb_api_secret'] = getenv('fb_api_secret');
 $app['fb_redirect_login_url'] = getenv('fb_redirect_login_url');
 
-include __DIR__ . '/../env.php';
+$envFile = __DIR__ . '/../env.php';
+if(file_exists($envFile)) include $envFile;
 
 $app->register(new MonologServiceProvider(), array(
 	'monolog.logfile' => __DIR__.'/../var/logs/silex_dev.log',
